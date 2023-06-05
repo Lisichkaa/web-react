d0c75cca0b4c98da4afef346afb42638dc33c6a5
Lisichka <zhu.ivanova@mail.ru>
2023-06-05 23:47:54 +0700

    change image

--- src/db/db.js
+++ src/db/db.js
@@ -6,10 +6,10 @@ import cakeImg from "../images/cake.jpg";
 
 export function getData() {
   return [
-    { title: "Белочка", amount: 100, Image: squirrelImg, id: 1, typeId: 0 },
+    { title: "Белочка", amount: 100, Image: coffeeImg, id: 1, typeId: 0 },
     { title: "Вопросики", amount: 1000, Image: questionImg,id: 2, typeId: 0 },
     { title: "Булочка с листиком", amount: 5000, Image: cakeImg, id: 3, typeId: 0 },
-    { title: "Кофе", amount: 10000, Image: coffeeImg , id: 4, typeId: 0 },
+    { title: "Кофе", amount: 10000, Image: squirrelImg , id: 4, typeId: 0 },
     { title: "Булочка с листиком", amount: 50000, Image: cakeImg, id: 3, typeId: 0},
     { title: "23 награды на профиль", amount: 6600, Image: squirrelImg, id: 5, typeId: 1 },
   ];

bfdad76828e985540a681df6f1063f6619604f76
Lisichkaa <76997431+Lisichkaa@users.noreply.github.com>
2023-06-05 03:18:06 -0700

    edit name of main button

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -94,7 +94,7 @@ const ProductList = () => {
         if(newItems.length >= 1) {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${tg.initDataUnsafe?.user.id}`
+                text: `Купить ${tg.initDataUnsafe.user}`
             })
         } else {
             tg.MainButton.hide();            
@@ -118,7 +118,7 @@ const ProductList = () => {
         if(newItems.length >= 1) {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${tg.initDataUnsafe?.user.id}`
+                text: `Купить ${tg.initDataUnsafe.user}`
             })
         } else {
             tg.MainButton.hide();            

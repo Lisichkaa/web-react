5cbf96359195c559c12c88971902a08d4d6c359e
Lisichka <zhu.ivanova@mail.ru>
2023-06-06 20:52:54 +0700

    edit cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,33 +4,32 @@ import {useNavigate} from 'react-router-dom'
 import ProductList from '../ProductList/ProductList';
 
 
-function Cart (totalAmount) {
+function Cart () {
 
   const {tg} = telegramUse();   
-  const addedItems = ProductList();
+  
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
      
-  const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            //totalAmount: totalAmount
-        }
-        tg.sendData(JSON.stringify(data));
-    }, [addedItems, totalAmount]) 
+  // const onSendData = useCallback(() => {
+  //       const data = {
+  //           products: addedItems,
+  //           totalAmount: totalAmount
+  //       }
+  //       tg.sendData(JSON.stringify(data));
+  //   }, [addedItems, totalAmount]) 
 
-  useEffect(() => {
-    tg.onEvent('mainButtonClicked', onSendData)
-      return () => {
-          tg.offEvent('mainButtonClicked', onSendData)
-      }
-  }, [onSendData])
+  // useEffect(() => {
+  //   tg.onEvent('mainButtonClicked', onSendData)
+  //     return () => {
+  //       tg.offEvent('mainButtonClicked', onSendData)
+  //   }
+  // }, [onSendData])
 
 
   return (
       <div className={"cart"}>        
-            <h3>Your order <button onClick={goBack}>Edit</button></h3> 
-           
+            <h3>Your order <button onClick={goBack}>Edit</button></h3>            
       </div>
     );
 };

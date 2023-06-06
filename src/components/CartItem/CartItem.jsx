d9ffaf90ac254a18ef3759fa26e1738f8b7b7684
Lisichka <zhu.ivanova@mail.ru>
2023-06-07 01:00:06 +0700

    add order items

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './CartItem.css';
 
-const CartItem = (addedItems) => {
+const CartItem = (product) => {
 
-    const {title, Image, amount, id, typeId, quantity} = addedItems.products;  
+    const {title, Image, amount, id, typeId, quantity} = product;  
 
     return (
         <div className="order_item_card">          
-          <div className="order_image__container">
+          {/* <div className="order_image__container">
             <img src={Image} alt={title} />
-          </div>
+          </div> */}
           <h4 className="order_item__title">
             {title} x{quantity}
           </h4>

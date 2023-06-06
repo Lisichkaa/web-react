29ef8d31ccf813861e5bc3a88d23897f73727947
Lisichka <zhu.ivanova@mail.ru>
2023-06-06 20:49:37 +0700

    edit cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './Cart.css';
 import {useNavigate} from 'react-router-dom'
+import ProductList from '../ProductList/ProductList';
 
 
-function Cart (addedItems) {
+function Cart (totalAmount) {
 
+  const {tg} = telegramUse();   
+  const addedItems = ProductList();
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
      
-  // const onSendData = useCallback(() => {
-  //       const data = {
-  //           products: addedItems,
-  //           totalAmount: totalAmount
-  //       }
-  //       tg.sendData(JSON.stringify(data));
-  //   }, [addedItems, totalAmount]) 
+  const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            //totalAmount: totalAmount
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [addedItems, totalAmount]) 
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+      return () => {
+          tg.offEvent('mainButtonClicked', onSendData)
+      }
+  }, [onSendData])
+
 
   return (
       <div className={"cart"}>        

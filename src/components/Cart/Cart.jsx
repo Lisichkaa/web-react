9128bb3268f324da2ca15a76120b9064d735a833
Lisichka <zhu.ivanova@mail.ru>
2023-06-06 18:18:24 +0700

    edit cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Cart.css';
+import {useNavigate} from 'react-router-dom'
+
 
 const Cart = () => {
-    return (
+
+  const goBack = () => navigate(-1);
+
+  return (
       <div className={"cart"}>
+        <button onClick={goBack}>Edit</button>
             <h3>Your order</h3>
       </div>
     );

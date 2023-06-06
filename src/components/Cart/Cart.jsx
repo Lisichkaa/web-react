9f5580538205387b7733059ffab8496017d928c8
Lisichka <zhu.ivanova@mail.ru>
2023-06-06 22:04:16 +0700

    edit senddata

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,27 +10,24 @@ function Cart (totalAmount) {
   const goBack = () => navigate(-1);
      
   //это метод для отправки данных в бот сообщением
-  // const onSendData = useCallback(() => {
-  //       const data = {
-  //           products: addedItems,
-  //           totalAmount: totalAmount
-  //       }
-  //       tg.sendData(JSON.stringify(data));
-  //   }, [addedItems, totalAmount]) 
+  const onSendData = useCallback(() => {       
+        tg.sendData(JSON.stringify(obj2));
+    }, [obj2]) 
 //здесь ожидаем эвент нажатие на главную кнопку
-  // useEffect(() => {
-  //   tg.onEvent('mainButtonClicked', onSendData)
-  //     return () => {
-  //       tg.offEvent('mainButtonClicked', onSendData)
-  //   }
-  // }, [onSendData])
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+      return () => {
+        tg.offEvent('mainButtonClicked', onSendData)
+    }
+  }, [onSendData])
+
   var obj = JSON.parse(sessionStorage['cart']);  
   var obj2 = sessionStorage['cart'];
   
 
   return (
     <div class="order_header_wrap">      
-      <h2 class="order_header">Your Order{obj}-{obj2}</h2>
+      <h2 class="order_header">Your Order</h2>
       <div class="order_edit"> 
            <h3><button onClick={goBack}>Edit</button></h3>       
       </div>
